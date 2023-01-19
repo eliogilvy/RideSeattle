4cc89117204e4e043ec66d5e353353760c092c96
Eli Ogilvy <eogilvy@seattleu.edu>
2023-01-19 14:07:09 -0800

    Added specific api call

--- ride_seattle/lib/OneBusAway/routes.dart
+++ ride_seattle/lib/OneBusAway/routes.dart
@@ -8,4 +8,8 @@ class Routes {
   static String getAgencies() {
     return 'http://api.pugetsound.onebusaway.org/api/where/agencies-with-coverage.xml?key=${Key.oneBusAway}';
   }
+
+  static String getRoutes(String id) {
+    return 'https://api.pugetsound.onebusaway.org/api/where/routes-for-agency/$id.xml?key=${Key.oneBusAway}';
+  }
 }
