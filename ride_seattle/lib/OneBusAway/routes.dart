c110d1d48b3b6cdd1b6afd6ad308defca4543a48
Eli Ogilvy <eogilvy@seattleu.edu>
2023-01-19 12:54:03 -0800

    Routes for OneBusAway API

---
+++ ride_seattle/lib/OneBusAway/routes.dart
@@ -0,0 +1,11 @@
+import 'keys.dart';
+
+class Routes {
+  static String getStops(String id) {
+    return 'http://api.pugetsound.onebusaway.org/api/where/stop-ids-for-agency/$id.xml?key=${Key.oneBusAway}';
+  }
+
+  static String getAgencies() {
+    return 'http://api.pugetsound.onebusaway.org/api/where/agencies-with-coverage.xml?key=${Key.oneBusAway}';
+  }
+}
