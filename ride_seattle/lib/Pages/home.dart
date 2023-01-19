8cbb9a7b972397b1581c7cf93f78ec715ea26178
Mark Taylor <taylor26@seattleu.edu>
2023-01-19 13:57:52 -0800

    added homescreen import to home.dart

--- ride_seattle/lib/Pages/home.dart
+++ ride_seattle/lib/Pages/home.dart
@@ -4,6 +4,8 @@ import 'package:ride_seattle/OneBusAway/test_data.dart';
 import 'package:ride_seattle/Pages/maps.dart';
 import 'package:ride_seattle/Pages/sign_in.dart';
 
+import 'homescreen.dart';
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
@@ -13,7 +15,7 @@ class Home extends StatelessWidget {
     final routerDelegate = BeamerDelegate(
       locationBuilder: RoutesLocationBuilder(
         routes: {
-          "/": (context, state, data) => const MapsPage(),
+          "/": (context, state, data) => const HomeScreen(),
         },
       ),
     );
