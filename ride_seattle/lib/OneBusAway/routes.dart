d5845f84fc62cb5a15e9137a8aec7d753898bf30
Eli Ogilvy <eogilvy@seattleu.edu>
2023-01-19 18:29:05 -0800

    Added some new api routes

--- ride_seattle/lib/OneBusAway/routes.dart
+++ ride_seattle/lib/OneBusAway/routes.dart
@@ -1,8 +1,8 @@
 import 'keys.dart';
 
 class Routes {
-  static String getStops(String id) {
-    return 'http://api.pugetsound.onebusaway.org/api/where/stop-ids-for-agency/$id.xml?key=${Key.oneBusAway}';
+  static String getStopsForRoute(String id) {
+    return 'http://api.pugetsound.onebusaway.org/api/where/stops-for-route/$id.xml?key=${Key.oneBusAway}';
   }
 
   static String getAgencies() {
@@ -11,5 +11,13 @@ class Routes {
 
   static String getRoutes(String id) {
     return 'https://api.pugetsound.onebusaway.org/api/where/routes-for-agency/$id.xml?key=${Key.oneBusAway}';
+  }
+
+  static String getStop(String id) {
+    return 'http://api.pugetsound.onebusaway.org/api/where/stop/$id.xml?key=${Key.oneBusAway}';
+  }
+
+  static String getStopIdsForAgency(String id) {
+    return 'http://api.pugetsound.onebusaway.org/api/where/stop-ids-for-agency/$id.xml?key=${Key.oneBusAway}';
   }
 }
