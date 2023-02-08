a317636a3d0046594163c3e888fdb12f7f3e83b1
Eli Ogilvy <eogilvy@seattleu.edu>
2023-02-08 14:50:52 -0800

    Updated Stop class

--- ride_seattle/lib/classes/stop.dart
+++ ride_seattle/lib/classes/stop.dart
@@ -13,7 +13,7 @@ class Stop {
   final String code;
   final int locationType;
   final List<String> routeIds;
-  ArrivalAndDeparture? arrivalAndDeparture;
+  Map<String, ArrivalAndDeparture> _arrivalAndDeparture = {};
 
   Stop({
     required this.stopId,
@@ -24,84 +24,73 @@ class Stop {
     required this.code,
     required this.locationType,
     required this.routeIds,
-    this.arrivalAndDeparture,
   });
 
-  void getArrivalAndDeparture() async {
-    Response res = await get(Uri.parse(Routes.getArrivalAndDeparture(stopId)));
+  Map<String, ArrivalAndDeparture> get arrivalAndDeparture =>
+      _arrivalAndDeparture;
+
+  List<ArrivalAndDeparture> get arrivalAndDepartureList =>
+      _arrivalAndDeparture.values.toList();
+
+  Future<void> getArrivalAndDeparture() async {
+    print("stop $stopId");
+    Response res =
+        await get(Uri.parse(Routes.getArrivalsAndDepartures(stopId)));
     final document = XmlDocument.parse(res.body);
-    final arrivalAndDepartureNode =
-        document.findElements("arrivalAndDeparture").first;
+    var arrivalAndDepartureNode =
+        document.findAllElements("arrivalAndDeparture");
 
-    final routeId = arrivalAndDepartureNode.findElements("routeId").first.text;
-    final tripId = arrivalAndDepartureNode.findElements("tripId").first.text;
-    final serviceDate = int.parse(
-        arrivalAndDepartureNode.findElements("serviceDate").first.text);
-    final id = arrivalAndDepartureNode.findElements("stopId").first.text;
-    final stopSequence = int.parse(
-        arrivalAndDepartureNode.findElements("stopSequence").first.text);
-    final blockTripSequence = int.parse(
-        arrivalAndDepartureNode.findElements("blockTripSequence").first.text);
-    final routeShortName =
-        arrivalAndDepartureNode.findElements("routeShortName").first.text;
-    final routeLongName =
-        arrivalAndDepartureNode.findElements("routeLongName").first.text;
-    final tripHeadsign =
-        arrivalAndDepartureNode.findElements("tripHeadsign").first.text;
-    final arrivalEnabled =
-        arrivalAndDepartureNode.findElements("arrivalEnabled").first.text ==
-            "true";
-    final departureEnabled =
-        arrivalAndDepartureNode.findElements("departureEnabled").first.text ==
-            "true";
-    final scheduledArrivalTime = int.parse(arrivalAndDepartureNode
-        .findElements("scheduledArrivalTime")
-        .first
-        .text);
-    final scheduledDepartureTime = int.parse(arrivalAndDepartureNode
-        .findElements("scheduledDepartureTime")
-        .first
-        .text);
-    final frequency =
-        arrivalAndDepartureNode.findElements("frequency").first.text;
-    final predicted =
-        arrivalAndDepartureNode.findElements("predicted").first.text == "true";
-    final predictedArrivalTime = int.parse(arrivalAndDepartureNode
-        .findElements("predictedArrivalTime")
-        .first
-        .text);
-    final predictedDepartureTime = int.parse(arrivalAndDepartureNode
-        .findElements("predictedDepartureTime")
-        .first
-        .text);
-    final distanceFromStop = double.parse(
-        arrivalAndDepartureNode.findElements("distanceFromStop").first.text);
-    final numberOfStopsAway = int.parse(
-        arrivalAndDepartureNode.findElements("numberOfStopsAway").first.text);
-    final tripStatus =
-        arrivalAndDepartureNode.findElements("tripStatus").first.text;
+    for (var ad in arrivalAndDepartureNode) {
+      final routeId = ad.findElements("routeId").first.text;
+      final tripId = ad.findElements("tripId").first.text;
+      final serviceDate = int.parse(ad.findElements("serviceDate").first.text);
+      final id = ad.findElements("stopId").first.text;
+      final stopSequence =
+          int.parse(ad.findElements("stopSequence").first.text);
+      final blockTripSequence =
+          int.parse(ad.findElements("blockTripSequence").first.text);
+      final routeShortName = ad.findElements("routeShortName").first.text;
+      final tripHeadsign = ad.findElements("tripHeadsign").first.text;
+      final arrivalEnabled =
+          ad.findElements("arrivalEnabled").first.text == "true";
+      final departureEnabled =
+          ad.findElements("departureEnabled").first.text == "true";
+      final scheduledArrivalTime =
+          int.parse(ad.findElements("scheduledArrivalTime").first.text);
+      final scheduledDepartureTime =
+          int.parse(ad.findElements("scheduledDepartureTime").first.text);
+      final predicted = ad.findElements("predicted").first.text == "true";
+      final predictedArrivalTime =
+          int.parse(ad.findElements("predictedArrivalTime").first.text);
+      final predictedDepartureTime =
+          int.parse(ad.findElements("predictedDepartureTime").first.text);
+      final distanceFromStop =
+          double.parse(ad.findElements("distanceFromStop").first.text);
+      final numberOfStopsAway =
+          int.parse(ad.findElements("numberOfStopsAway").first.text);
+      final tripStatus = ad.findElements("tripStatus").first.text;
 
-    arrivalAndDeparture = ArrivalAndDeparture(
-      routeId: routeId,
-      tripId: tripId,
-      serviceDate: serviceDate,
-      stopId: id,
-      stopSequence: stopSequence,
-      blockTripSequence: blockTripSequence,
-      routeShortName: routeShortName,
-      routeLongName: routeLongName,
-      tripHeadsign: tripHeadsign,
-      arrivalEnabled: arrivalEnabled,
-      departureEnabled: departureEnabled,
-      scheduledArrivalTime: scheduledArrivalTime,
-      scheduledDepartureTime: scheduledDepartureTime,
-      frequency: frequency,
-      predicted: predicted,
-      predictedArrivalTime: predictedArrivalTime,
-      predictedDepartureTime: predictedDepartureTime,
-      distanceFromStop: distanceFromStop,
-      numberOfStopsAway: numberOfStopsAway,
-      tripStatus: tripStatus,
-    );
+      print("adding $routeId");
+      _arrivalAndDeparture[tripId] = ArrivalAndDeparture(
+        routeId: routeId,
+        tripId: tripId,
+        serviceDate: serviceDate,
+        stopId: id,
+        stopSequence: stopSequence,
+        blockTripSequence: blockTripSequence,
+        routeShortName: routeShortName,
+        tripHeadsign: tripHeadsign,
+        arrivalEnabled: arrivalEnabled,
+        departureEnabled: departureEnabled,
+        scheduledArrivalTime: scheduledArrivalTime,
+        scheduledDepartureTime: scheduledDepartureTime,
+        predicted: predicted,
+        predictedArrivalTime: predictedArrivalTime,
+        predictedDepartureTime: predictedDepartureTime,
+        distanceFromStop: distanceFromStop,
+        numberOfStopsAway: numberOfStopsAway,
+        tripStatus: tripStatus,
+      );
+    }
   }
 }
