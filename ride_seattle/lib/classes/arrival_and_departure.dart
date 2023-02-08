129b2e70186b67568d412e2ca540788ed1dd480f
Eli Ogilvy <eogilvy@seattleu.edu>
2023-02-08 14:51:06 -0800

    Updated arrival and departure info

--- ride_seattle/lib/classes/arrival_and_departure.dart
+++ ride_seattle/lib/classes/arrival_and_departure.dart
@@ -6,13 +6,11 @@ class ArrivalAndDeparture {
   final int stopSequence;
   final int blockTripSequence;
   final String routeShortName;
-  final String routeLongName;
   final String tripHeadsign;
   final bool arrivalEnabled;
   final bool departureEnabled;
   final int scheduledArrivalTime;
   final int scheduledDepartureTime;
-  final String frequency;
   final bool predicted;
   final int predictedArrivalTime;
   final int predictedDepartureTime;
@@ -28,13 +26,11 @@ class ArrivalAndDeparture {
     required this.stopSequence,
     required this.blockTripSequence,
     required this.routeShortName,
-    required this.routeLongName,
     required this.tripHeadsign,
     required this.arrivalEnabled,
     required this.departureEnabled,
     required this.scheduledArrivalTime,
     required this.scheduledDepartureTime,
-    required this.frequency,
     required this.predicted,
     required this.predictedArrivalTime,
     required this.predictedDepartureTime,
