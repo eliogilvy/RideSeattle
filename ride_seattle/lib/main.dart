7429342bf8e2865fd153797c7602373a4e28456d
Eli Ogilvy <eogilvy@seattleu.edu>
2023-02-09 18:25:38 -0800

    RideApp

--- ride_seattle/lib/main.dart
+++ ride_seattle/lib/main.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'Pages/current_location_screen.dart';
-import 'Pages/home.dart';
 import 'provider/state_info.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   var stateInfo = StateInfo();
@@ -16,27 +14,24 @@ void main() async {
       providers: [
         ChangeNotifierProvider.value(value: stateInfo),
       ],
-      child: rideApp(),
+      child: const RideApp(),
     ),
   );
 }
 
-
 final _router = GoRouter(
-    initialLocation: '/',
-    routes:[
-      GoRoute(
-        path:'/',
-        builder: (context, state) => const CurrentLocationScreen(),
-        routes: [],
-      ),
-    ],
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const CurrentLocationScreen(),
+      routes: [],
+    ),
+  ],
 );
 
-
-class rideApp extends StatelessWidget {
-
-  const rideApp({super.key});
+class RideApp extends StatelessWidget {
+  const RideApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -46,4 +41,3 @@ class rideApp extends StatelessWidget {
     );
   }
 }
-
