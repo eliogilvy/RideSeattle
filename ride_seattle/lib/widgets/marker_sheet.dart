fc237fd3802c6d442107ef3b8ddacbf3ff735fc7
Eli Ogilvy <eogilvy@seattleu.edu>
2023-02-11 16:25:50 -0800

    updated marker sheet

--- ride_seattle/lib/widgets/marker_sheet.dart
+++ ride_seattle/lib/widgets/marker_sheet.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:ride_seattle/widgets/arrival_and_departure_list.dart';
+import 'package:ride_seattle/widgets/route_name.dart';
+
+import '../provider/state_info.dart';
 
 class MarkerSheet extends StatelessWidget {
   final GoogleMapController controller;
@@ -8,16 +12,26 @@ class MarkerSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final stateInfo = Provider.of<StateInfo>(context, listen: false);
     return DraggableScrollableSheet(
       builder: (BuildContext context, ScrollController scrollController) {
         return Container(
-          height: 100,
-          color: Colors.lightBlueAccent,
-          child: Center(
-            child: ArrivalAndDepartureList(
-              scrollController: scrollController,
-              controller: controller,
-            ),
+          color: Theme.of(context).colorScheme.background,
+          child: Column(
+            children: [
+              RouteName(text: stateInfo.currentStopInfo.name),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 17, 0, 0),
+                  child: Center(
+                    child: ArrivalAndDepartureList(
+                      scrollController: scrollController,
+                      controller: controller,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         );
       },
