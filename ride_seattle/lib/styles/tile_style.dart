df7b629a19aef9bbf04e465074f7f0a88309632b
Eli Ogilvy <eogilvy@seattleu.edu>
2023-02-11 16:25:12 -0800

    added some textsyling

---
+++ ride_seattle/lib/styles/tile_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TileStyle {
+  static TextStyle routeNumberStyle() {
+    return const TextStyle(
+      fontFamily: 'route',
+      fontSize: 26,
+    );
+  }
+
+  static TextStyle routeNameStyle() {
+    return const TextStyle(
+      fontFamily: 'route',
+      fontSize: 20,
+    );
+  }
+}
