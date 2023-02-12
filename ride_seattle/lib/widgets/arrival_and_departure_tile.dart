64ccdd80a3b840f5ddf7a3f98d74c8e0d136e09e
Eli Ogilvy <eogilvy@seattleu.edu>
2023-02-11 16:25:40 -0800

    Updated stop info tile

--- ride_seattle/lib/widgets/arrival_and_departure_tile.dart
+++ ride_seattle/lib/widgets/arrival_and_departure_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:ride_seattle/classes/arrival_and_departure.dart';
+import 'package:ride_seattle/widgets/route_box.dart';
 
 import '../provider/state_info.dart';
 
@@ -15,27 +16,60 @@ class ArrivalAndDepartureTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final stateInfo = Provider.of<StateInfo>(context, listen: false);
     return ListTile(
-      title: Text(adInfo.routeShortName.toString()),
-      trailing: IconButton(
-        icon: const Icon(
-          Icons.bus_alert,
-        ),
-        onPressed: () async {
-          if (adInfo.tripStatus != null) {
-            await stateInfo.addMarker('vehicle', adInfo.routeShortName,
-                adInfo.tripStatus!.position, stateInfo.getVehicleInfo,
-                iconFilepath: 'assets/images/icons8-bus-96.png');
-            controller.animateCamera(
-              CameraUpdate.newCameraPosition(
-                CameraPosition(
-                  target: adInfo.tripStatus!.position,
-                  zoom: 16,
-                ),
-              ),
-            );
-          }
-        },
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          RouteBox(text: adInfo.routeShortName),
+          Expanded(
+            child: Container(),
+          ),
+          RouteBox(text: getPredictedArrivalTime(adInfo)),
+          IconButton(
+            tooltip: "Find my vehicle",
+            icon: const Icon(
+              Icons.directions_bus,
+            ),
+            onPressed: () {
+              if (adInfo.tripStatus != null) {
+                findBus(stateInfo);
+              }
+            },
+          ),
+        ],
       ),
     );
+  }
+
+  void findBus(StateInfo stateInfo) async {
+    await stateInfo.addMarker('vehicle', adInfo.routeShortName,
+        adInfo.tripStatus!.position, stateInfo.getVehicleInfo,
+        iconFilepath: 'assets/images/icons8-bus-96.png');
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: adInfo.tripStatus!.position,
+          zoom: 16,
+        ),
+      ),
+    );
+  }
+
+  String getPredictedArrivalTime(ArrivalAndDeparture adInfo) {
+    int prediction;
+    if (adInfo.predictedArrivalTime == 0) {
+      prediction = adInfo.scheduledArrivalTime;
+    } else {
+      prediction = adInfo.predictedArrivalTime;
+    }
+    int now = DateTime.now().millisecondsSinceEpoch;
+
+    int difference = prediction - now;
+    int differenceInMinutes = difference ~/ 60000;
+    if (differenceInMinutes == 0) {
+      return "NOW";
+    } else if (differenceInMinutes < 0) {
+      return "Departed";
+    }
+    return "${differenceInMinutes.toString()} min";
   }
 }
