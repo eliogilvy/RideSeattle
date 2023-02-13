1b6585ac9d9c772a7eb8253702058322cfec5d29
Eli Ogilvy <eogilvy@seattleu.edu>
2023-02-13 10:29:59 -0800

    Updated theme

--- ride_seattle/lib/styles/theme.dart
+++ ride_seattle/lib/styles/theme.dart
@@ -3,10 +3,20 @@ import 'package:flutter/material.dart';
 class RideSeattleTheme {
   static ThemeData theme() {
     return ThemeData(
-      colorScheme: ColorScheme.fromSwatch().copyWith(
-        primary: Colors.lightBlue,
-        secondary: Colors.blue,
-        background: Colors.lightBlue,
+      primarySwatch: Colors.blue,
+      primaryColor: Colors.blue[900],
+      primaryColorLight: Colors.blue[100],
+      primaryColorDark: Colors.blue[800],
+      canvasColor: Colors.white,
+      scaffoldBackgroundColor: Colors.white,
+      cardColor: Colors.blue[50],
+      dividerColor: Colors.grey[400],
+      secondaryHeaderColor: Colors.blue[200],
+      dialogBackgroundColor: Colors.white,
+      indicatorColor: Colors.blue[900],
+      hintColor: Colors.grey[400],
+      buttonTheme: const ButtonThemeData(
+        textTheme: ButtonTextTheme.primary,
       ),
     );
   }
