b7eb65c410ce700c002f786e12323e9f72720c52
Eli Ogilvy <eogilvy@seattleu.edu>
2023-03-02 18:11:59 -0800

    loading widget with 10 sec timeout

---
+++ ride_seattle/lib/widgets/loading.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LoadingWidget extends StatefulWidget {
+  const LoadingWidget({super.key});
+
+  @override
+  State<LoadingWidget> createState() => _LoadingWidgetState();
+}
+
+class _LoadingWidgetState extends State<LoadingWidget> {
+  final int timeout = 10;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: timeout)).then((value) {
+      if (mounted) {
+        setState(() {
+          _loading = false;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _loading
+        ? const CircularProgressIndicator()
+        : const Text("No data available, try tapping on the stop again.");
+  }
+}
