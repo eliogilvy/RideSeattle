eb3163f6fb7c91fa2ccd47fed3d61c8fcdcb712c
Eli Ogilvy <eogilvy@seattleu.edu>
2023-03-02 18:12:24 -0800

    ui updates for text overflow

--- ride_seattle/lib/widgets/route_box.dart
+++ ride_seattle/lib/widgets/route_box.dart
@@ -2,32 +2,51 @@ import 'package:flutter/material.dart';
 
 import '../styles/tile_style.dart';
 
-class RouteBox extends StatelessWidget {
+class RouteBox extends StatefulWidget {
   final String text;
-  const RouteBox({super.key, required this.text});
+  const RouteBox({super.key, required this.text, required this.maxW});
+  final double maxW;
+
+  @override
+  State<RouteBox> createState() => _RouteBoxState();
+}
+
+class _RouteBoxState extends State<RouteBox> {
+  bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 40,
-      constraints: const BoxConstraints(
-        minWidth: 40,
-      ),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black,
-          width: 1.5,
+    return IntrinsicWidth(
+      child: Container(
+        constraints:
+            BoxConstraints(maxHeight: 40, minWidth: 40, maxWidth: widget.maxW),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 1.5,
+          ),
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(15),
         ),
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(3, 0, 3, 0),
-        child: Align(
-          alignment: Alignment.center,
-          child: Text(
-            text,
-            style: TileStyle.routeNumberStyle(),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(3, 0, 3, 0),
+          child: Tooltip(
+            message: widget.text,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isExpanded = !_isExpanded;
+                });
+              },
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  widget.text,
+                  style: TileStyle.routeNumberStyle(),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
           ),
         ),
       ),
