fe89834a7a33743c91f6f252d6f8bc7ff8d37d0a
Mark Taylor <taylor26@seattleu.edu>
2023-03-03 14:08:28 -0800

    added long press feautre on arrival_and_departure_tile.dart in bottomsheet.
    Long press now will add a route to the favorites list which will be saved in local storage.
    
    Changed arrival_and_departure_tile.dart to be a stateful widget

--- ride_seattle/lib/widgets/arrival_and_departure_tile.dart
+++ ride_seattle/lib/widgets/arrival_and_departure_tile.dart
@@ -6,13 +6,82 @@ import 'package:ride_seattle/provider/route_provider.dart';
 import 'package:ride_seattle/widgets/route_box.dart';
 
 import '../classes/stop.dart';
+import '../provider/localStorageProvider.dart';
 import '../provider/state_info.dart';
 
-class ArrivalAndDepartureTile extends StatelessWidget {
+class ArrivalAndDepartureTile extends StatefulWidget {
+
   final ArrivalAndDeparture adInfo;
   final GoogleMapController controller;
+
   const ArrivalAndDepartureTile(
       {super.key, required this.adInfo, required this.controller});
+
+  @override
+  State<ArrivalAndDepartureTile> createState() => _ArrivalAndDepartureTileState();
+}
+
+
+class _ArrivalAndDepartureTileState extends State<ArrivalAndDepartureTile>{
+
+  var localStorage;
+  var favoriteRoutes;
+
+  Offset _tapPosition = Offset.zero;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => _onAfterBuild(context));
+  }
+
+  void _onAfterBuild(BuildContext context) {
+    localStorage = Provider.of<localStorageProvider>(context, listen: false);
+
+    try{
+      localStorage.loadData();
+    }
+    catch (e){
+      print(e.toString());
+    }
+    favoriteRoutes = localStorage.getFavoriteRoutes();
+  }
+
+  void _getTapPosition(TapDownDetails tapPos){
+    final RenderBox renderBox = context.findRenderObject() as RenderBox;
+
+    setState((){
+      _tapPosition = renderBox.globalToLocal(tapPos.globalPosition);
+    });
+
+  }
+
+  void _showContextMenu(context, List<LatLng> routeStops) async{
+    final RenderObject? overlay = Overlay.of(context)?.context.findRenderObject();
+    final result = await showMenu(
+        context: context,
+        position: RelativeRect.fromRect(
+            Rect.fromLTWH(_tapPosition.dx, _tapPosition.dy, 10, 10),
+            Rect.fromLTWH(0, 0, overlay!.paintBounds.size.width,
+                overlay!.paintBounds.size.height)),
+        items: [
+          const PopupMenuItem(
+            value: "favorited",
+            child: Text('Add to favorites'),
+          ),
+        ]
+    );
+
+    if(result == "favorited"){
+      print("route favorited");
+      //add polylines to local database
+
+      localStorage.addRoute(routeStops);
+      print("route added - arrival/departure tile");
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,33 +91,48 @@ class ArrivalAndDepartureTile extends StatelessWidget {
       title: Row(
         children: [
           RouteBox(
-            text: adInfo.routeShortName,
+            text: widget.adInfo.routeShortName,
             maxW: 150,
           ),
           Expanded(
             child: Container(),
           ),
           RouteBox(
-            text: getPredictedArrivalTime(adInfo),
+            text: getPredictedArrivalTime(widget.adInfo),
             maxW: double.infinity,
           ),
-          IconButton(
-            tooltip: "Find my vehicle",
-            icon: const Icon(
-              Icons.directions_bus,
-            ),
-            onPressed: () async {
-              if (adInfo.tripStatus != null) {
-                //get all the stops for the current route
-                List<LatLng> routeStops =
-                    await stateInfo.getRoutePolyline(adInfo.routeId);
-                //add those stops to the routeProvider
 
-                routeProvider.setPolyLines(routeStops);
+          GestureDetector(
+            onTapDown: (position){
+              _getTapPosition(position);
 
-                findBus(stateInfo);
-              }
             },
+            child: InkWell(
+              //tooltip: "Find my vehicle",
+              onTap: () async {
+                if (widget.adInfo.tripStatus != null) {
+                  //get all the stops for the current route
+                  List<LatLng> routeStops =
+                  await stateInfo.getRoutePolyline(widget.adInfo.routeId);
+                  //add those stops to the routeProvider
+
+                  routeProvider.setPolyLines(routeStops);
+
+                  findBus(stateInfo);
+                }
+              },
+              onLongPress: () async {
+                //add routes to favorite
+                print("long press");
+                //TODO change this so we are getting the route ID and route number and storing that locally
+                List<LatLng> routeStops =
+                await stateInfo.getRoutePolyline(widget.adInfo.routeId);
+                _showContextMenu(context, routeStops);
+              },
+              child: Ink(
+                child: const Icon(Icons.directions_bus),
+              ),
+            ),
           ),
         ],
       ),
@@ -56,13 +140,14 @@ class ArrivalAndDepartureTile extends StatelessWidget {
   }
 
   void findBus(StateInfo stateInfo) {
-    stateInfo.addMarker('vehicle', adInfo.routeShortName,
-        adInfo.tripStatus!.position, stateInfo.getVehicleInfo,
+    stateInfo.addMarker('vehicle', widget.adInfo.routeShortName,
+        widget.adInfo.tripStatus!.position, stateInfo.getVehicleInfo,
         iconFilepath: 'assets/images/icons8-bus-96.png');
-    controller.animateCamera(
+
+    widget.controller.animateCamera(
       CameraUpdate.newCameraPosition(
         CameraPosition(
-          target: adInfo.tripStatus!.position,
+          target: widget.adInfo.tripStatus!.position,
           zoom: 16,
         ),
       ),
@@ -88,3 +173,5 @@ class ArrivalAndDepartureTile extends StatelessWidget {
     return "${differenceInMinutes.toString()} min";
   }
 }
+
+
