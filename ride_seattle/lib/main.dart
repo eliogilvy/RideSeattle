3be0d9b4e081c3ce6f88453e65f5c097d4ec5d58
Mark Taylor <taylor26@seattleu.edu>
2023-03-03 14:00:47 -0800

    added local storage hive provider to main and init hive

--- ride_seattle/lib/main.dart
+++ ride_seattle/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+
 import 'package:ride_seattle/provider/route_provider.dart';
 import 'package:ride_seattle/styles/theme.dart';
 import 'provider/state_info.dart';
@@ -8,16 +9,29 @@ import 'package:firebase_core/firebase_core.dart';
 import 'Pages/check_auth.dart';
 import 'package:flutter/services.dart';
 
+import 'package:ride_seattle/provider/localStorageProvider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+late Box favRouteBox;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  //initialize hive offline storage
+  await Hive.initFlutter();
+
 
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => StateInfo()),
         ChangeNotifierProvider(create: (context) => RouteProvider()),
+        ListenableProvider<localStorageProvider>(create: (context) =>
+            localStorageProvider(
+                favRouteBox: favRouteBox)
+        ),
       ],
       child: const RideApp(),
     ),
