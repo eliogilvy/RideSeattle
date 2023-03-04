3b692d3a0e365f5be1361f299019dcda7fc4a5ca
Eli Ogilvy <eogilvy@seattleu.edu>
2023-03-04 13:09:19 -0800

    Theme update

--- ride_seattle/lib/styles/theme.dart
+++ ride_seattle/lib/styles/theme.dart
@@ -3,21 +3,25 @@ import 'package:flutter/material.dart';
 class RideSeattleTheme {
   static ThemeData theme() {
     return ThemeData(
-      primarySwatch: Colors.blue,
-      primaryColor: Colors.blue[900],
-      primaryColorLight: Colors.blue[100],
-      primaryColorDark: Colors.blue[800],
-      canvasColor: Colors.white,
-      scaffoldBackgroundColor: Colors.white,
-      cardColor: Colors.blue[50],
-      dividerColor: Colors.grey[400],
-      secondaryHeaderColor: Colors.blue[200],
-      dialogBackgroundColor: Colors.white,
-      indicatorColor: Colors.blue[900],
-      hintColor: Colors.grey[400],
-      buttonTheme: const ButtonThemeData(
-        textTheme: ButtonTextTheme.primary,
-      ),
-    );
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue[900],
+        primaryColorLight: Colors.blue[100],
+        primaryColorDark: Colors.blue[800],
+        canvasColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+        cardColor: Colors.blue[50],
+        dividerColor: Colors.grey[400],
+        secondaryHeaderColor: Colors.blue[200],
+        dialogBackgroundColor: Colors.white,
+        indicatorColor: Colors.blue[900],
+        hintColor: Colors.grey[400],
+        buttonTheme: const ButtonThemeData(
+          textTheme: ButtonTextTheme.primary,
+        ),
+        textTheme: const TextTheme(
+            bodyMedium: TextStyle(
+          fontFamily: 'route',
+          fontSize: 18,
+        )));
   }
 }
