075de771ee19b5cb759697c8544f7245f7b5b6cd
Mark Taylor <taylor26@seattleu.edu>
2023-03-12 16:57:56 -0700

    Added skyline jpeg to login screen

--- ride_seattle/lib/Pages/login_screen.dart
+++ ride_seattle/lib/Pages/login_screen.dart
@@ -118,25 +118,33 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final fire = Provider.of<FireProvider>(context);
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset('assets/images/logo.png'),
-                _emailField('Email', emailController),
-                _passwordField('Password', passwordController),
-                _errorMessage(),
-                _submitButton(fire.auth),
-                _loginOrRegisterButton(),
-              ],
+      appBar: AppBar(
+        title: _title(),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/skyline.jpg'),
+                fit: BoxFit.cover,
+              )
+          ),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 175),
+              Image.asset('assets/images/logo.png'),
+              _emailField('Email', emailController),
+              _passwordField('Password', passwordController),
+              _errorMessage(),
+              _submitButton(fire.auth),
+              _loginOrRegisterButton(),
+            ],
             ),
           ),
         ),
-      ),
     );
   }
 }
